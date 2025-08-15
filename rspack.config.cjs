caddf2d6f5fc3c1ce0109195179789c0788e0730
IRONM00N <me@ironmoon.dev>
2025-08-15 00:21:29 -0400

    feat: migrate to rspack

---
+++ rspack.config.cjs
@@ -0,0 +1,104 @@
+const path = require("path");
+
+/** @type {import('@rspack/core').Configuration[]} */
+module.exports = [
+  // ESM build
+  {
+    name: "esm",
+    mode: "production",
+    target: ["web", "es2022"],
+    entry: {
+      pyret: "./src/pyret.ts",
+      api: "./src/api.ts",
+      "default-rpcs": "./src/default-rpcs.ts",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+      library: { type: "module" },
+    },
+    experiments: {
+      outputModule: true,
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".js"],
+      fallback: {
+        fs: false,
+        path: false,
+        process: false,
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: "builtin:swc-loader",
+              options: {
+                jsc: {
+                  parser: { syntax: "typescript" },
+                  target: "es2022",
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+  // CJS build
+  {
+    name: "cjs",
+    mode: "production",
+    target: ["web", "es2022"],
+    entry: {
+      pyret: "./src/pyret.ts",
+      api: "./src/api.ts",
+      "default-rpcs": "./src/default-rpcs.ts",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].cjs",
+      module: false,
+      chunkFormat: "commonjs",
+      library: { type: "commonjs2" },
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".js"],
+      fallback: {
+        fs: false,
+        path: false,
+        process: false,
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: "builtin:swc-loader",
+              options: {
+                jsc: {
+                  parser: { syntax: "typescript" },
+                  target: "es2022",
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    externals: {
+      fs: "commonjs fs",
+      path: "commonjs path",
+      process: "commonjs process",
+    },
+  },
+];
+
